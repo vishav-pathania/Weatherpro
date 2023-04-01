c131256e5f85aadd18b3265f74b51290657767d1
vishav-pathania <vishavpathania40@gmail.com>
2023-04-01 12:04:00 +0530

    rendering data and taking user location

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 import "./style.css"
 import { getweather } from "./weather"
- getweather(10,10,Intl.DateTimeFormat().resolvedOptions().timeZone)
- .then(renderWeather)
- .catch(e => {
-    console.error(e)
-    alert("Error getting Weather!")
+import { ICON_MAP } from "./iconMap"
+
+navigator.geolocation.getCurrentPosition(positionSuccess, positionError)
+
+function positionSuccess({ coords }){
+   getweather(coords.latitude,coords.longitude,Intl.DateTimeFormat().resolvedOptions().timeZone)
+   .then(renderWeather)
+   .catch(e => {
+   console.error(e)
+   alert("Error getting Weather!")
  })
+}
+
+function positionError(){
+   alert("There was an error getting your location. Please allow us to use your location and refresh the page.")
+}
+
 
  function renderWeather({ current, daily, hourly }){
-    renderCurrentWeather(current)
-    // renderDailyWeather(daily)
-    // renderHourlyWeather(hourly)
-    document.body.classList.remove("blurred")
+   renderCurrentWeather(current)
+   renderDailyWeather(daily)
+   renderHourlyWeather(hourly)
+   document.body.classList.remove("blurred")
  }
 
 function setValue(selector, value, {parent=document}={}){
@@ -19,7 +30,7 @@ function setValue(selector, value, {parent=document}={}){
 }
 
 function getIconUrl(iconCode){
-    return `icons/${iconCode}.svg`
+   return `icons/${ICON_MAP.get(iconCode)}.svg`
 }
 
 const currentIcon = document.querySelector("[data-current-icon")
@@ -32,4 +43,43 @@ const currentIcon = document.querySelector("[data-current-icon")
     setValue("current-fl-low", current.lowFeelsLike)
     setValue("current-wind", current.windSpeed)
     setValue("current-precip", current.precip)
+ }
+
+const DAY_FORMATTER = new Intl.DateTimeFormat(undefined, { weekday: "long" })
+
+const dailySection = document.querySelector("[data-day-section]")
+const dayCardTemplate = document.getElementById("day-card-template")
+
+ function renderDailyWeather(daily){
+   dailySection.innerHTML=""
+   daily.forEach(day => {
+      const element = dayCardTemplate.content.cloneNode(true)
+      setValue("temp", day.maxTemp, { parent: element })
+      setValue("date", DAY_FORMATTER.format(day.timestamp), {parent: element})
+      element.querySelector("[data-icon]").src= getIconUrl(day.iconCode)
+      dailySection.append(element)
+   })
+ }
+
+ const HOUR_FORMATTER = new Intl.DateTimeFormat(undefined, { hour: "numeric" })
+
+ const hourlySection = document.querySelector("[data-hour-section]")
+ const hourRowTemplate = document.getElementById("hour-row-template")
+
+ function renderHourlyWeather(hourly){
+   hourlySection.innerHTML = ""
+   hourly.forEach(hour => {
+      const element = hourRowTemplate.content.cloneNode(true)
+      setValue("temp", hour.temp, { parent: element })
+      setValue("fl-temp", hour.feelsLike, { parent: element })
+      setValue("wind", hour.windSpeed, { parent: element })
+      setValue("precip", hour.precip, { parent: element })
+      setValue("day", DAY_FORMATTER.format(hour.timestamp), { parent: element })
+      setValue("time", HOUR_FORMATTER.format(hour.timestamp), { parent: element })
+      element.querySelector("[data-icon]").src= getIconUrl(hour.iconCode)
+      hourlySection.append(element)
+
+   })
+      
+   
  }
